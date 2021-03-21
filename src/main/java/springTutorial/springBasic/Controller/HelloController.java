6da9207f606629452d205a5e14acb73568df6fd3
praivesi <praivesi@gmail.com>
2021-03-21 16:00:59 +0900

    Add hello.html, HelloController

---
+++ src/main/java/springTutorial/springBasic/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package springTutorial.springBasic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Transferred Data");
+        return "hello";
+    }
+}
